25a6df3ad03d35600f43b8c8f0c0414233290fe9
Kassio 2022 <kassiogluten@gmail.com>
2022-01-22 23:52:20 -0300

    revalidate only posts/slug

--- src/pages/404.js
+++ src/pages/404.js
@@ -11,12 +11,12 @@ export default function Custom404() {
   return (
     <Layout>
       <Helmet>
-        <title>404 | Page not found</title>
+        <title>404 | Página não encontrada</title>
         <meta name="robots" content="noindex, nofollow" />
       </Helmet>
       <Section>
         <Container className={styles.center}>
-          <h1>Page Not Found</h1>
+          <h1>Página não encontrada</h1>
           <span>The page you were looking for could not be found.</span>
           <p>
             <Link href="/">
--- src/pages/categories/[slug].js
+++ src/pages/categories/[slug].js
@@ -27,7 +27,6 @@ export async function getStaticProps({ params = {} } = {}) {
       category,
       posts,
     },
-    revalidate: 60 * 10,
   };
 }
 
@@ -45,6 +44,6 @@ export async function getStaticPaths() {
 
   return {
     paths,
-    fallback: 'blocking',
+    fallback: false,
   };
 }
